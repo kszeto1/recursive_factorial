67eff737603cc1429721b9b3582eabfb93ff15a2
Kenny Szeto <kenny.szeto1@gmail.com>
2017-08-24 20:08:26 -0700

    Finally recursive

--- recursive_factorial.py
+++ recursive_factorial.py
@@ -2,17 +2,10 @@
 # returns the number of ways to arrange the input number of items.
 
 def factorial(n):
-    base = 1
-    total = 1
-    range_of_n = range(1, n + 1)
-    for numbers in range_of_n:
-        if numbers == 2:
-            total = numbers * (numbers - 1)
-        else:
-            total *= numbers
-    return total
     if n == 0:
-        return base
+        return 1
+    else:
+        return n * factorial(n - 1)
 
 
 
